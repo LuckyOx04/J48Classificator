83a7ad4778e6f423f89a5178f8488329c19be435
LuckyOx04 <ivailon27@gmail.com>
2025-11-21 16:03:02 +0200

    Placed the initial controls in the main window.

---
+++ J48UserInterface/SolutionTreeWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace J48UserInterface;
+
+public partial class SolutionTreeWindow : Window
+{
+    public SolutionTreeWindow()
+    {
+        InitializeComponent();
+    }
+}
